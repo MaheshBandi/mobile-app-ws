c17128c7f7bd890f1a09fff009c2b53b8a7ff14e
Mahesh Bandi <mbandi1985@gmail.com>
2020-07-26 19:40:49 +0530

    Added a Custom Exception Exception Handler

--- src/main/java/com/appsdelevoperblog/app/ws/ui/controller/UserController.java
+++ src/main/java/com/appsdelevoperblog/app/ws/ui/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.appsdelevoperblog.app.ws.ui.controller;
 
+import com.appsdelevoperblog.app.ws.exceptions.UserServiceException;
 import com.appsdelevoperblog.app.ws.service.UserService;
 import com.appsdelevoperblog.app.ws.shared.dto.UserDto;
 import com.appsdelevoperblog.app.ws.ui.model.request.UserDetailsRequestModel;
+import com.appsdelevoperblog.app.ws.ui.model.response.ErrorMessages;
 import com.appsdelevoperblog.app.ws.ui.model.response.UserRest;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,9 +33,10 @@ public class UserController {
     }
     @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE},
                  produces = {MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE})
-    public ResponseEntity<UserRest> createUser(@RequestBody UserDetailsRequestModel userDetails){
+    public ResponseEntity<UserRest> createUser(@RequestBody UserDetailsRequestModel userDetails) throws UserServiceException{
 
         UserRest returnValue = new UserRest();
+        if(userDetails.getFirstName().isEmpty()) throw new UserServiceException(ErrorMessages.MISSING_REQUIRED_FIELDS.getErrorMessage());
         UserDto userDto = new UserDto();
         BeanUtils.copyProperties(userDetails,userDto);
         UserDto createdUser = userService.createUser(userDto);
